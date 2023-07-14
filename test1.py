f91ce947b8dd0e393cba71be7ef9397338704908
LalitPathak27 <104768342+LalitPathak27@users.noreply.github.com>
2023-07-14 20:22:25 +0530

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,6 @@
+a = 333333.3333339028899
+b = 333333.3333339028898
+if b > a:
+    print("b is grater than a")
+elif a == b:
+    print("a and b are equal")
---
+++ test2.py
@@ -0,0 +1,6 @@
+def my_function(fname):
+    print(fname + " refsnes")
+
+my_function("Emil")
+my_function("tobidas")
+my_function("linus")   
